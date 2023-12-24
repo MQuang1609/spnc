5af9881de90005cb37fbc5092155bef5cd3df08d
MQuang1609 <lunganh31@gmail.com>
2023-12-24 10:51:22 +0700

    second

--- assest/js/game-scene.js
+++ assest/js/game-scene.js
@@ -11,21 +11,21 @@ class GameScene extends Phaser.Scene {
   endIndexText = null;
 
   constructor() {
-    super('GameScene');
+    super("GameScene");
   }
 
   preload() {
-    console.log('preload');
+    console.log("preload");
   }
 
   create() {
-    this.game.events.on('start', ({ values, value }) =>
+    this.game.events.on("start", ({ values, value }) =>
       this.start({ values, value })
     );
   }
 
   update() {
-    console.log('update');
+    console.log("update");
   }
 
   start({ values, value }) {
@@ -68,8 +68,8 @@ class GameScene extends Phaser.Scene {
   }
 
   draw() {
-    this.valueText = this.add.text(50, 50, `Searching value: ${this.value}`, {
-      color: '#fff',
+    this.valueText = this.add.text(50, 50, `Giá trị đang tìm: ${this.value}`, {
+      color: "#fff",
       fontSize: 12,
     });
 
@@ -88,12 +88,12 @@ class GameScene extends Phaser.Scene {
         50 + index * 30 + 12,
         400 - value * 3 - 10,
         `${value}`,
-        { color: '#fff', fontSize: 12 }
+        { color: "#fff", fontSize: 12 }
       );
       valueText.setOrigin(0.5, 0.5);
 
       const indexText = this.add.text(50 + index * 30 + 12, 410, `${index}`, {
-        color: '#fff',
+        color: "#fff",
         fontSize: 12,
       });
       indexText.setOrigin(0.5, 0.5);
@@ -113,8 +113,8 @@ class GameScene extends Phaser.Scene {
       console.log(bar.barNoteText);
       bar.barNoteText?.destroy(true);
       bar.rect.setFillStyle(0xffffff, 1);
-      bar.valueText.setColor('#fff');
-      bar.indexText.setColor('#fff');
+      bar.valueText.setColor("#fff");
+      bar.indexText.setColor("#fff");
     });
   }
 
@@ -137,7 +137,7 @@ class GameScene extends Phaser.Scene {
 
   drawBarRange(start, end) {
     for (let i = start; i <= end; i++) {
-      this.drawBarNote(i, '', '#ff0000');
+      this.drawBarNote(i, "", "#ff0000");
     }
   }
 
@@ -146,8 +146,8 @@ class GameScene extends Phaser.Scene {
   }
 
   async highlight(code) {
-    this.game.events.emit('highlight', code);
-    await this.delay(800);
+    this.game.events.emit("highlight", code);
+    await this.delay(3000);
   }
 
   async findIndex() {
@@ -164,29 +164,39 @@ class GameScene extends Phaser.Scene {
       let middle = Math.floor((start + end) / 2);
 
       this.startIndexText?.destroy(true);
-      this.startIndexText = this.add.text(50, 110, `Start index: ${start}`, {
-        color: '#fff',
-        fontSize: 12,
-      });
+      this.startIndexText = this.add.text(
+        50,
+        110,
+        `Vị trí đầu mảng: ${start}`,
+        {
+          color: "#fff",
+          fontSize: 12,
+        }
+      );
       this.middleIndexText?.destroy(true);
-      this.middleIndexText = this.add.text(50, 130, `Middle index: ${middle}`, {
-        color: '#fff',
-        fontSize: 12,
-      });
+      this.middleIndexText = this.add.text(
+        50,
+        130,
+        `Vị trí giữa mảng: ${middle}`,
+        {
+          color: "#fff",
+          fontSize: 12,
+        }
+      );
       this.endIndexText?.destroy(true);
-      this.endIndexText = this.add.text(50, 150, `End index: ${end}`, {
-        color: '#fff',
+      this.endIndexText = this.add.text(50, 150, `Vị trí cuối mảng: ${end}`, {
+        color: "#fff",
         fontSize: 12,
       });
 
-      this.drawBarNote(middle, 'Middle', '#00ff00');
+      this.drawBarNote(middle, "Giá trị giữa mảng", "#00ff00");
       this.middleValueText?.destroy(true);
       this.middleValueText = this.add.text(
         50,
         70,
-        `Middle value: ${this.values[middle]}`,
+        `Giá trị giữa mảng: ${this.values[middle]}`,
         {
-          color: '#fff',
+          color: "#fff",
           fontSize: 12,
         }
       );
@@ -196,13 +206,13 @@ class GameScene extends Phaser.Scene {
         90,
         `${
           this.values[middle] < this.value
-            ? 'Middle < value'
+            ? "Giá trị giữa mảng < Giá trị đang tìm"
             : this.values[middle] > this.value
-            ? 'Middle > value'
-            : 'Middle = value'
+            ? "Giá trị giữa mảng > Giá trị đang tìm"
+            : "Giá trị giữa mảng = Giá trị đang tìm"
         }`,
         {
-          color: '#fff',
+          color: "#fff",
           fontSize: 12,
         }
       );
@@ -211,7 +221,7 @@ class GameScene extends Phaser.Scene {
       if (this.values[middle] === this.value) {
         await this.highlight(5);
         this.clearBarNote();
-        this.drawBarNote(middle, 'Result', '#00ff00');
+        this.drawBarNote(middle, "Đã tìm thấy", "#00ff00");
         return middle;
       } else {
         await this.highlight(6);
@@ -225,7 +235,7 @@ class GameScene extends Phaser.Scene {
         }
       }
 
-      await this.delay(1000);
+      await this.delay(2700);
     }
 
     this.clearBarNote();
@@ -236,15 +246,25 @@ class GameScene extends Phaser.Scene {
   async run() {
     const index = await this.findIndex();
     if (index === -1) {
-      this.resultText = this.add.text(350, 470, `Not found!`, {
-        color: '#fff',
-        fontSize: 14,
-      });
+      this.resultText = this.add.text(
+        350,
+        470,
+        `Không tìm thấy giá trị cần tìm!`,
+        {
+          color: "#fff",
+          fontSize: 14,
+        }
+      );
     } else {
-      this.resultText = this.add.text(350, 470, `Index found: ${index}`, {
-        color: '#fff',
-        fontSize: 14,
-      });
+      this.resultText = this.add.text(
+        350,
+        470,
+        `Giá trị được tìm thấy ở vị trí: ${index}`,
+        {
+          color: "#fff",
+          fontSize: 14,
+        }
+      );
     }
     this.resultText.setOrigin(0.5, 0.5);
   }
